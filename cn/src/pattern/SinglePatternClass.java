4808a8c21ba7ce530aee3c17dc7c3e7546e82263
bearduncle <15615735246@163.com>
2019-05-10 13:37:25 +0800

    单例模式

---
+++ cn/src/pattern/SinglePatternClass.java
@@ -0,0 +1,58 @@
+package pattern;
+
+/*
+ * 单例模式
+ * 整个系统中只创建一个类实例
+ * 要求：外部不能实例化这个类
+ * 1.私有化成员变量
+ * 2.私有化构造方法
+ * 3.提供对外获取对象实例的接口
+ * */
+
+//懒汉式- -线程不安全
+public class SinglePatternClass {
+    private static SinglePatternClass singlePatternClass = null;
+
+    private SinglePatternClass() {
+    }
+
+    public static SinglePatternClass getSingleClass() {
+        if (singlePatternClass == null) {
+            //假如两个线程都已经执行完if判断
+            //然后每个线程继续执行下面的构造方法就会出现两个对象
+            singlePatternClass = new SinglePatternClass();
+        }
+        return singlePatternClass;
+    }
+}
+
+//饿汉式 - - 线程安全
+class SinglePatternClass2 {
+    private static SinglePatternClass2 singlePatternClass = new SinglePatternClass2();
+
+    private SinglePatternClass2() {
+    }
+
+    public static SinglePatternClass2 getSingleClass() {
+        return singlePatternClass;
+    }
+}
+
+//懒汉式双重锁
+class SinglePatternClass3 {
+    private static SinglePatternClass3 singlePatternClass = null;
+
+    private SinglePatternClass3() {
+    }
+
+    public static SinglePatternClass3 getSingleClass() {
+        if (singlePatternClass == null) {
+            synchronized (SinglePatternClass3.class) {
+                if (singlePatternClass == null) {
+                    singlePatternClass = new SinglePatternClass3();
+                }
+            }
+        }
+        return singlePatternClass;
+    }
+}
