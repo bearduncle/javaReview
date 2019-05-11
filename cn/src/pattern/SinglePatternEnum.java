e4de2cd5765795682c323d029429c69d7b0ca57e
xianfeng <1107719738@qq.com>
2019-05-11 22:42:01 +0800

    枚举实现的单例模式

---
+++ cn/src/pattern/SinglePatternEnum.java
@@ -0,0 +1,10 @@
+package pattern;
+
+
+//枚举类实现的单例模式 - - 可以避免反射，序列化和反序列化的问题
+public enum SinglePatternEnum {
+    SINGLE_PATTERN_ENUM;
+    public static SinglePatternEnum getSingle(){
+        return SINGLE_PATTERN_ENUM;
+    }
+}
