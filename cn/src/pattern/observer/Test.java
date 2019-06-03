78bb87ae97e4514167b5c2a8a647585fc3e6370f
bearduncle <15615735246@163.com>
2019-06-03 16:28:20 +0800

    补充注释

--- cn/src/pattern/observer/Test.java
+++ cn/src/pattern/observer/Test.java
@@ -1,8 +1,13 @@
 package pattern.observer;
 
 import java.util.Date;
-import java.util.UUID;
-
+/*
+* 观察者模式
+* 可以看成报社(主题)- -订阅者(观察者)的一个系统
+* 主题方是更新数据后通知所有观察者的一方,而在主题方注册的观察者都可以接收到主题最新的数据
+* 比如:招聘软件(主题)更新一些招聘信息通知给找工作的人(观察者)
+*      QQ群聊:每个群里面的成员既是主题又是观察者,游戏里面的喇叭喊话
+* */
 public class Test {
     public static void main(String[] args) {
         ObjectData data=new ObjectData(new Date(),"多云");
